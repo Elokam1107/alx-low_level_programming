6b42ec1f4ceac64792d08d759d1e09a2372cbfa6
Emmanuel Chimelokam <elokam1107@gmail.com>
2022-10-18 05:52:17 +0100

    if

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 if no error, non zero if error.
+ */
+int main(void)
+{
+	char i;
+	char a;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+	       putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
