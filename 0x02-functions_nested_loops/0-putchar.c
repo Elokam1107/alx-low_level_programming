200ebfa4200e11d2b22f9d3e2f46e73584ed0c2a
Emmanuel Chimelokam <elokam1107@gmail.com>
2022-10-30 20:57:51 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * main - prints _putchar
+ * main - print _putchar
  *
  * Return: 0
  */
  int main(void)
  {
- 	char i
- 	
- 	for (i = '_putchar')
- 	{
- 	print (i);
- 	}
+ 	_putchar('_');
+ 	_putchar('p');
+ 	_putchar('u');
+ 	_putchar('t');
+ 	_putchar('c');
+ 	_putchar('h');
+ 	_putchar('a');
+ 	_putchar('r');
+ 	_putchar('\n');
  	return(0);
  }
