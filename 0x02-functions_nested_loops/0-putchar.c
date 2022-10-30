86538aba3ddf57d650ef9cdeeb7a6d7c41a62fd5
Emmanuel Chimelokam <elokam1107@gmail.com>
2022-10-30 18:41:36 +0100

    start

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+#include <main.h>
 
+/**
+ * main - prints _putchar
+ *
+ * Return: 0
+ */
+ int main(void)
+ {
+ 	char i
+ 	
+ 	for (i = '_putchar')
+ 	{
+ 	print (i);
+ 	}
+ 	return(0);
+ }
